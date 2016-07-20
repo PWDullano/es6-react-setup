fa322c3b69870f2d60a4fe9b357371011a7dda1c
Patrick Dullano <PWDullano@gmail.com>
2016-07-20 12:04:30 -0600

    Added hello's to App.js in JavaScript, JSX and in a stateless function component.

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+
+//JavaScript
+// class App extends React.Component {
+//   render(){
+//     return React.createElement('h1', null, 'Herro World' )
+//   }
+// }
+
+//JSX
 class App extends React.Component {
   render(){
-    return <div>Herro Herro</div>
+    return <h1>Herro Herro</h1>
   }
 }
+
+//Stateless function component
+// const App = () => <h1>Herro Everybody</h1>
 
 export default App
